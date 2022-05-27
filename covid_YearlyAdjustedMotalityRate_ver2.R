3e82ed268d05f3003e52db0a7b02fb625717d6bd
igproj-fusion <myajima0202@gmail.com>
2022-05-27 13:03:23 +0900

    Update covid_YearlyAdjustedMotalityRate_ver2.R

--- covid_YearlyAdjustedMotalityRate_ver2.R
+++ covid_YearlyAdjustedMotalityRate_ver2.R
@@ -25,6 +25,10 @@ mutate_cond <- function(.data, condition, ..., envir = parent.frame()) {
 
 FirstD <- as.Date("2022/01/05")
 
+#
+# Deaths of Japan & Saitama
+#
+
 URL1 <- "https://covid19.mhlw.go.jp/public/opendata/deaths_detail_cumulative_weekly.csv"
 read.csv(URL1, header = F) %>%
   slice(-1, -2) %>%
@@ -41,6 +45,9 @@ Diff[is.na(Diff)] <- 0
 JapanDeath <- Diff[1:10] + Diff[11:20]
 SaitamaDeath <- Diff[21:30] + Diff[31:40]
 
+#
+# Deaths of Chiba
+#
 
 URL2 <- "https://raw.githubusercontent.com/igproj-fusion/covid19_data/main/covid_chiba_deaths.csv"
 read.csv(URL2) %>%
@@ -57,14 +64,20 @@ read.csv(URL2) %>%
   select(n) -> ChibaDeath
 
 
+#
+# Calculation of Age-adjusted Mortality
+#
+
 p <- read.csv("https://raw.githubusercontent.com/igproj-fusion/covid19_data/main/Jpop47_20220101.csv")
 JapanPop <- as.numeric(p[1,-1])
 ChibaPop <- as.numeric(p[13,-1])
 SaitamaPop <- as.numeric(p[12,-1])
 
+
 q <- read.csv("https://raw.githubusercontent.com/igproj-fusion/covid19_data/main/H27ModelPop.csv", 
               header = FALSE)
 modelPop <- q$V2 * 1000
+
 
 JapanMR <- sum(JapanDeath / JapanPop * modelPop) / sum(modelPop) * 100000
 ChibaMR <- sum(ChibaDeath / ChibaPop * modelPop) / sum(modelPop) * 100000
