2e03e48accf543ded4ac1e377254e2e5ff70cc4e
igproj-fusion <myajima0202@gmail.com>
2022-07-02 20:05:55 +0900

    Update covid19_infect_death_JP.R

--- covid19_infect_death_JP.R
+++ covid19_infect_death_JP.R
@@ -7,45 +7,61 @@ library(ggpmisc)
 
 
 URL <- "https://www3.nhk.or.jp/n-data/opendata/coronavirus/nhk_news_covid19_prefectures_daily_data.csv"
+
+pop47 <- read.csv("https://raw.githubusercontent.com/igproj-fusion/covid19_data/main/pop47.csv", header = T)
+
 St <- as.Date("2021-12-15")
-
-pop47 <- read.csv("https://raw.githubusercontent.com/igproj-fusion/covid19_data/main/pop47.csv")
-
 read.csv(URL) %>%
-  select(date = "日付", 
-         pref = "都道府県名", 
-         cum_c = "各地の感染者数_累計", 
-         cum_d = "各地の死者数_累計") %>%
+  select(date = "日付", pref = "都道府県名", cump = "各地の感染者数_累計", cumd = "各地の死者数_累計") %>%
   mutate(date = as.Date(date)) %>%
-  filter(date %in% c(St - 1, max(date))) %>%
+  filter(date >= St) %>%
   group_by(pref) %>%
-  mutate(cum_c = cum_c - min(cum_c)) %>%
-  mutate(cum_d = cum_d - min(cum_d)) %>%
-  filter(date == max(date)) %>%
-  left_join(pop47, by = c("pref" = "PREF")) %>%
-  mutate(C_perP = cum_c / POP * 100000) %>%
-  mutate(D_perP = cum_d / POP * 100000) %>%
-  select(date, pref, C_perP, D_perP) -> df
+  mutate(cump = cump - min(cump)) %>%
+  mutate(cumd = cumd - min(cumd)) %>%
+  mutate(perP = 0) %>%
+  mutate(perD = 0) -> df
 
-g <- ggplot(df, aes(x = C_perP, y = D_perP))
+
+for(i in 1:47){
+  df$perP[df$pref == pop47$PREF[i]] <- df$cump[df$pref == pop47$PREF[i]] / pop47$POP[i] *100000
+  df$perD[df$pref == pop47$PREF[i]] <- df$cumd[df$pref == pop47$PREF[i]] / pop47$POP[i] *100000
+}
+
+last <- data.frame(df[df$date == max(df$date),])
+last2 <- left_join(last, pop47, by=c("pref" = "PREF"))
+
+cor(last2$perP, last2$perD)^2
+
+cor(last2$cump, last2$POP)
+cor.test(last2$perP, last2$perD)
+
+
+g <- ggplot(last2, aes(x = perP, y = perD))
 g <- g + geom_smooth(method = "lm", color = "lightblue", alpha = 0.3)
-g <- g + geom_point(color = "blue")
-g <- g + geom_text_repel(aes(label = pref),
-                         max.overlaps = 20, segment.size = 0.2)
-g <- g + scale_y_continuous(limits = c(min = 0.0, max = max(df$D_perP)),
+g <- g + geom_point(color="blue")
+g <- g + geom_text_repel(aes(x = perP, y = perD, label = pref),
+                         max.overlaps=20, segment.size = 0.2)
+g <- g + scale_y_continuous(breaks = c(0,  5.0, 10, 15.0, 20.0, 25.0),
+                            limits=c(min=0.0, max=25.0),
                             labels = scales::comma)
+
 g <- g + stat_poly_eq(formula = y ~ x,
                       aes(label = paste(stat(eq.label),
                                         stat(rr.label),
                                         stat(adj.rr.label),
+                                        #stat(f.value.label),
+                                        #stat(p.value.label),
+                                        #stat(AIC.label),
+                                        #stat(BIC.label),
                                         sep = "*\", \"*")),
-                      eq.with.lhs = "italic(hat(y))~` = `~",
+                      eq.with.lhs = "italic(hat(y))~`=`~",
                       eq.x.rhs = "~italic(z)",
                       parse = TRUE, size = 5)
+#g <- g + scale_x_log10() + scale_y_log10()
 g <- g + theme_light()
-g <- g + theme(plot.margin = unit(c(2, 2, 2, 2), "lines"))
-g <- g + labs(title = "第6波の累積感染者数と累積死亡者数（人口10万人あたり）",
-              subtitle = paste0(St, "〜", df$date[1]),
+g <- g + theme(plot.margin= unit(c(2, 2, 2, 2), "lines"))
+g <- g + labs(title = "第6波以降の累積感染者数と累積死亡者数（人口10万人あたり）",
+              subtitle = paste0("2021-12-15〜", df$date[nrow(df)]),
               x = "Positives per 100,000 people", 
               y = "Deaths per 100,000 people",
               caption = paste("Source: ", URL))
